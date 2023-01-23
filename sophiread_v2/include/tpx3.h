dd9fef9dfa1c2e0e406b82229b9eec8246108012
Chen Zhang <zhangc@ornl.gov>
2023-01-23 15:04:30 -0500

    add the reader function

--- sophiread_v2/include/tpx3.h
+++ sophiread_v2/include/tpx3.h
@@ -1,3 +1,6 @@
+#include <fstream>
+#include <vector>
+
 /**
  * @brief Class to store the data of a single hit of a charged particle 
  *        hitting the timepix3 sensor.
@@ -5,16 +8,26 @@
  */
 class Hit {
     public:
-        Hit(const int x, const int y, const int tot, const int toa, const int ftoa, const unsigned int tof);
-        ~Hit();
-        int getX() const;
-        int getY() const;
-        int getTOT() const;
-        int getTOA() const;
-        unsigned int getTOF() const;
-        double getTOF_ns() const;
-        double getTOA_ns() const;
-        double getTOT_ns() const;
+        Hit(
+            const int x,
+            const int y,
+            const int tot,
+            const int toa,
+            const int ftoa,
+            const unsigned int tof,
+            const int spidertime);
+
+        int getX() const {return m_x;};
+        int getY() const {return m_y;};
+        int getTOT() const {return m_tot;};
+        int getTOA() const {return m_toa;};
+        int getFTOA() const {return m_ftoa;};
+        int getSPIDERTIME() const {return m_spidertime;};
+        unsigned int getTOF() const {return m_tof;};
+
+        double getTOF_ns() const {return m_tof * m_scale_to_ns;};
+        double getTOA_ns() const {return m_toa * m_scale_to_ns;};
+        double getTOT_ns() const {return m_tot * m_scale_to_ns;};
 
     private:
         // raw packet directly read from tpx3.
@@ -23,8 +36,11 @@ class Hit {
         const int m_toa;  // time of arrival (40MHz clock, 14 bit)
         const int m_ftoa;  // fine time of arrival (640MHz clock, 4 bit)
         const unsigned int m_tof;
+        const int m_spidertime; // time from the spider board
 
         // scale factor that converts time to ns
         // NOTE: 40 MHz clock is used for the timepix3, hence 25 ns per tick.
         double m_scale_to_ns = 25.0;
 };
+
+std::vector<Hit> readTimepix3RawData(const std::string& filepath);
--- sophiread_v2/src/tpx3.cpp
+++ sophiread_v2/src/tpx3.cpp
@@ -1,76 +1,112 @@
+#include <iostream>
 #include "tpx3.h"
 
-Hit::Hit(const int x, const int y, const int tot, const int toa, const int ftoa, const unsigned int tof) :
-    m_x(x), m_y(y), m_tot(tot), m_toa(toa), m_ftoa(ftoa), m_tof(tof) {}
+Hit::Hit(const int x, const int y, const int tot, const int toa, const int ftoa, const unsigned int tof, const int spidertime) :
+    m_x(x), m_y(y), m_tot(tot), m_toa(toa), m_ftoa(ftoa), m_tof(tof), m_spidertime(spidertime) {}
 
-/**
- * @brief Get the X coordinate of the hit.
- * 
- * @return int 
- */
-int Hit::getX() const {
-    return m_x;
-}
+std::vector<Hit> readTimepix3RawData(const std::string& filepath) {
+    int chip_layout_type = 0;
+    int data_packet_size = 0;
+    int data_packet_num = 0;
+    unsigned long *tdclast,mytdc;
+    unsigned  short pixaddr, dcol, spix, pix;
+    unsigned short *spider_time;
+    unsigned short *nTOT; //bytes 2,3, raw time over threshold
+    unsigned int *nTOA; //bytes 3,4,5,6, raw time of arrival
+    unsigned int *npixaddr;  //bytes 4,5,6,7
+    int x, y, tot, toa, ftoa, spidertime;
+    unsigned int tof;
 
-/**
- * @brief Get the Y coordinate of the hit.
- * 
- * @return int 
- */
-int Hit::getY() const {
-    return m_y;
-}
+    // Open the file
+    std::ifstream file(filepath, std::ios::binary);
+    if (!file.is_open()) {
+        throw std::runtime_error("Error opening file");
+    }
+    // Read the data from the file into a buffer
+    std::vector<unsigned char> buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
 
-/**
- * @brief Get the TOT of the hit.
- * 
- * @return int 
- */
-int Hit::getTOT() const {
-    return m_tot;
-}
+    // Process the buffer to extract the raw data
+    // (implementation of this will depend on the specific format of the TimePix3 data)
+    std::vector<Hit> hits;
+    std::cout << "File size: " << buffer.size() << std::endl;
 
-/**
- * @brief Get the TOA of the hit.
- * 
- * @return int 
- */
-int Hit::getTOA() const {
-    return m_toa;
-}
+    for (auto i = buffer.begin(); i != buffer.end(); i+=8) {
+        std::vector<char> char_array(i, std::next(i, 8));
+        if (char_array.size() != 8) {
+            std::cout << "EOF, insufficient bytes left." << std::endl;
+            break;
+        }
 
-/**
- * @brief Get the TOF of the hit.
- * 
- * @return unsigned int 
- */
-unsigned int Hit::getTOF() const {
-    return m_tof;
-}
+        // locate the data packet header
+        if (char_array[0]=='T' && char_array[1] == 'P' && char_array[2] == 'X') {
+            // get the size of the data packet
+            // NOTE:
+            // Sophiread_v1 is using a method that leads to negative data_packet_size and data_packet_num,
+            // therefore we are using the code from manufacture example, tpx3cam.cpp to get the
+            // data_packet_size.
+            data_packet_size = ((0xff & char_array[7]) << 8) | (0xff & char_array[6]);
+            data_packet_num = data_packet_size >> 3;  // every 8 (2^3) bytes is a data packet
 
-/**
- * @brief Get the TOF of the hit in ns.
- * 
- * @return const double 
- */
-double Hit::getTOF_ns() const {
-    return m_tof * m_scale_to_ns;
-}
+            // processing each data packet
+            for (auto j = 0; j < data_packet_num; j++){
+                i = std::next(i, 8);  //move the iterator to the next data packet
+                std::vector<char> data_packet(i, std::next(i, 8));
+                if (data_packet.size() != 8) {
+                    std::cout << "EOF, insufficient bytes left." << std::endl;
+                    break;
+                }
+                chip_layout_type = (int) char_array[4];
+                // extract the data from the data packet
+                if (data_packet[7] == 0x6F) {
+                    // TDC data packets
+                    tdclast=(unsigned long *)(&data_packet[0]);
+                    mytdc=(((*tdclast) >> 12) & 0x3fffffff);
+                } else if ((data_packet[7] & 0xF0) == 0x40) {
+                    // GDC
+                    // do nothing for now (unless we need the GDC count)
+                } else if ((data_packet[7] & 0xF0) == 0xb0) {
+                    // timing section
+                    spider_time = (unsigned short *)(&data_packet[0]);     // Spider time  (16 bits)
+                    nTOT=(unsigned short *)(&data_packet[2]);              // ToT          (10 bits)
+                    nTOA=(unsigned int *)(&data_packet[3]);                // ToA          (14 bits)
+                    ftoa = *nTOT & 0xF;                                    // fine ToA     (4 bits)
+                    tot = (*nTOT >> 4) & 0x3FF;
+                    toa = (*nTOA >> 6) & 0x3FFF;
+                    spidertime = 16384 * (*spider_time) + toa;
+                    tof = spidertime - mytdc;
 
-/**
- * @brief Get the TOA of the hit in ns.
- * 
- * @return const double 
- */
-double Hit::getTOA_ns() const {
-    return m_toa * m_scale_to_ns;
-}
+                    // position section
+                    npixaddr = (unsigned int *)(&data_packet[4]);          // Pixel address (14 bits)
+                    pixaddr = (*npixaddr >> 12) & 0xFFFF;
+                    dcol=((pixaddr & 0xFE00)>>8);
+                    spix=((pixaddr & 0x1F8) >>1);
+                    pix=pixaddr & 0x7;  
+                    x=dcol+(pix >> 2);                     // x coordinate
+                    y=spix+(pix & 0x3);                    // y coordinate
 
-/**
- * @brief Get the TOT of the hit in ns.
- * 
- * @return const double 
- */
-double Hit::getTOT_ns() const {
-    return m_tot * m_scale_to_ns;
+                    // update pixel position based on chip layout type
+                    if (chip_layout_type == 0) {               // single
+                        x += 260;
+                        y = y;
+                    } else if (chip_layout_type == 1) {        // double
+                        x = 255 - x + 260;
+                        y = 255 - y + 260;
+                    } else if (chip_layout_type == 2) {        // triple
+                        x = 255 - x;
+                        y = 255 - y + 260;
+                    } else {                                // quad
+                        x = x;
+                        y = y;
+                    }
+
+                    // make the hit
+                    hits.push_back(Hit(x, y, tot, toa, ftoa, tof, spidertime));
+                }
+            }
+        }
+    }
+
+    // Return the hits
+    return hits;
 }
