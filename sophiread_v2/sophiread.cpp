68556274fe771b783526dbc7fe87f0ecb047c8dd
Chen Zhang <zhangc@ornl.gov>
2023-01-25 16:43:51 -0500

    add CLI parsing

--- sophiread_v2/sophiread.cpp
+++ sophiread_v2/sophiread.cpp
@@ -5,17 +5,60 @@
  */
 #include <fstream>
 #include <iostream>
+#include <unistd.h>
 
 #include "tpx3.h"
 
 int main(int argc, char *argv[]) {
   auto input_tpx3 = std::string(argv[1]);
-  std::cout << "Processing " << input_tpx3 << std::endl;
-  auto hits = readTimepix3RawData(input_tpx3);
 
-  std::ofstream xy_file("sophiread_v2_converted.txt");
-  for (auto hit : hits) {
-    xy_file << hit.toString() << std::endl;
+  // processing command line arguments
+  std::string in_tpx3;
+  std::string out_hits;
+  bool verbose = false;
+
+  int opt;
+  while ((opt = getopt(argc, argv, "i:o:v")) != -1) {
+    switch (opt) {
+    case 'i':
+      in_tpx3 = optarg;
+      break;
+    case 'o':
+      out_hits = optarg;
+      break;
+    case 'v':
+      verbose = true;
+      break;
+    default:
+      std::cerr << "Usage: " << argv[0] << " [-i input_tpx3] [-o output_image] [-h output_hits] [-v]" << std::endl;
+      return 1;
+    }
   }
+
+  // verbose output
+  if (verbose) {
+    std::cout << "Input file: " << in_tpx3 << std::endl;
+    std::cout << "Output hits: " << out_hits << std::endl;
+  }
+
+  // read raw data
+  if (in_tpx3.empty()) {
+    std::cerr << "Error: no input file specified." << std::endl;
+    return 1;
+  }
+  auto hits = readTimepix3RawData(in_tpx3);
+
+  // write hits to file
+  // TODO: convert to output neutron events (x, y, tof) onces clustering and
+  //       peak fitting is implemented
+  if (!out_hits.empty()){
+    std::ofstream xy_file(out_hits);
+    for (auto hit : hits) {
+      xy_file << hit.toString() << std::endl;
+    }
+  }
+
+  // write image to file
+  // TODO: implement
   return 0;
 }
