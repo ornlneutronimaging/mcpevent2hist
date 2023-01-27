adb5745b2ef13109c88b1b780001912286d94366
Chen Zhang <zhangc@ornl.gov>
2023-01-27 15:08:32 -0500

    implement weighted centroiding

--- sophiread_v2/include/centroid.h
+++ sophiread_v2/include/centroid.h
@@ -2,20 +2,23 @@
 
 #include "peakfitting.h"
 
+/**
+ * @brief Using weighted centroid to predict the peak position
+ *
+ * x_peak = sum(x_i * tot_i) / sum(tot_i)
+ * y_peak = sum(y_i * tot_i) / sum(tot_i)
+ * tof_peak = sum(tof_i) / N
+ *
+ * NOTE: TOF distribution within a cluster should be small enough that algebraic
+ * mean is sufficient to approximating the TOF of the incident neutron.
+ */
 class Centroid : public PeakFittingAlgorithm {
  public:
   Centroid(bool weighted_by_tot = true) : weighted_by_tot(weighted_by_tot){};
 
-  // Pure virtual function for initializing the algorithm
-  void initialize() override;
-
-  // Pure virtual function for fitting the data
-  void fit(const std::vector<Hit>& data) override;
-
   // Pure virtual function for predicting the peak positions and parameters
   // predict -> (x, y, tof)
-  std::vector<std::tuple<double, double, double>> predict(
-      const std::vector<Hit>& data) override;
+  NeutronEvent fit(const std::vector<Hit>& data) override;
 
  private:
   bool weighted_by_tot = true;
--- sophiread_v2/src/centroid.cpp
+++ sophiread_v2/src/centroid.cpp
@@ -2,17 +2,20 @@
 
 #include <iostream>
 
-void Centroid::initialize() {
-  std::cout << "Centroid to be implemented." << std::endl;
-}
-
-void Centroid::fit(const std::vector<Hit>& data) {
-  std::cout << "Centroid to be implemented." << std::endl;
-}
-
-std::vector<std::tuple<double, double, double>> Centroid::predict(
-    const std::vector<Hit>& data) {
-  std::vector<std::tuple<double, double, double>> predictions = {};
-  std::cout << "Centroid to be implemented." << std::endl;
-  return predictions;
+NeutronEvent Centroid::fit(const std::vector<Hit>& data) {
+  // Calculate the average x, and y using tot as weight
+  double x = 0;
+  double y = 0;
+  double tof = 0;
+  double tot = 0;
+  for (const auto& hit : data) {
+    x += hit.getX() * hit.getTOT();
+    y += hit.getY() * hit.getTOT();
+    tof += hit.getTOF();
+    tot += hit.getTOT();
+  }
+  x /= tot;
+  y /= tot;
+  tof /= data.size();
+  return NeutronEvent(x, y, tof, data.size());
 }
