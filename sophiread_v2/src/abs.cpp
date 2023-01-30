044ae18b9365a69ea2be898a055bccee388c488f
Chen Zhang <zhangc@ornl.gov>
2023-01-30 13:44:00 -0500

    fixing vector update error

--- sophiread_v2/src/abs.cpp
+++ sophiread_v2/src/abs.cpp
@@ -14,8 +14,9 @@
  * @param data
  */
 void ABS::fit(const std::vector<Hit>& data) {
-  // reserve space for the cluster labels
-  clusterLabels_.reserve(data.size());
+  // reserve space for the cluster labels and initialize to -1
+  clusterLabels_.clear();
+  clusterLabels_.resize(data.size(), -1);
 
   // make a vector of clusters
   std::vector<Cluster> clusters;
@@ -27,9 +28,11 @@ void ABS::fit(const std::vector<Hit>& data) {
   // NOTE: this section can use openMP to speed up by chunking hits into smaller
   //       groups
   int max_label = numClusters_;
+  std::vector<int> cluster_sizes(numClusters_, 0);
   for (size_t i = 0; i < data.size(); i++) {
     auto& hit = data[i];
     int label = -1;  // assume it is a noise
+
     // loop over all clusters
     for (auto& cluster : clusters) {
       // case_0: cluster is empty, add hit to cluster and update cluster bounds
@@ -43,40 +46,28 @@ void ABS::fit(const std::vector<Hit>& data) {
         label = cluster.label;
         break;
       }
-      // case_1: cluster is full, reset cluster, increase the label and
-      // max_label, and add hit to cluster
-      else if (cluster.size == maxClusterSize_) {
-        cluster.size = 1;
-        cluster.x_min = hit.getX();
-        cluster.x_max = hit.getX();
-        cluster.y_min = hit.getY();
-        cluster.y_max = hit.getY();
-        cluster.label = max_label;
-        max_label++;
-        //
-        label = cluster.label;
-        break;
-      }
-      // case_2: hit is within the feather range of the cluster, add hit to
-      // cluster and update cluster bounds
-      else if (hit.getX() >= cluster.x_min - m_feature &&
-               hit.getX() <= cluster.x_max + m_feature &&
-               hit.getY() >= cluster.y_min - m_feature &&
-               hit.getY() <= cluster.y_max + m_feature) {
-        cluster.size++;
-        cluster.x_min = std::min(cluster.x_min, hit.getX());
-        cluster.x_max = std::max(cluster.x_max, hit.getX());
-        cluster.y_min = std::min(cluster.y_min, hit.getY());
-        cluster.y_max = std::max(cluster.y_max, hit.getY());
-        //
-        label = cluster.label;
-        break;
-      }
-      // case_3: hit is not within the feather range of the cluster, id as noise
-      else {
-        continue;
+      // case_1: cluster is not full, check if hit is within the feather range
+      // of the cluster, if yes, add hit to cluster and update cluster bounds,
+      // if not, id as noise
+      else if (cluster.size < maxClusterSize_) {
+        if (hit.getX() >= cluster.x_min - m_feature &&
+            hit.getX() <= cluster.x_max + m_feature &&
+            hit.getY() >= cluster.y_min - m_feature &&
+            hit.getY() <= cluster.y_max + m_feature) {
+          cluster.size++;
+          cluster.x_min = std::min(cluster.x_min, hit.getX());
+          cluster.x_max = std::max(cluster.x_max, hit.getX());
+          cluster.y_min = std::min(cluster.y_min, hit.getY());
+          cluster.y_max = std::max(cluster.y_max, hit.getY());
+          //
+          label = cluster.label;
+          break;
+        } else {
+          continue;
+        }
       }
     }
+    //
     clusterLabels_[i] = label;
   }
 }
@@ -90,20 +81,8 @@ void ABS::fit(const std::vector<Hit>& data) {
 std::vector<NeutronEvent> ABS::get_events(const std::vector<Hit>& data) {
   // Sanity check
   if (clusterLabels_.size() != data.size()) {
-    std::cout << "ERROR: cluster labels size does not match data size, call "
-                 "fit first!"
-              << std::endl;
-    return std::vector<NeutronEvent>();
-  }
-
-  // Create peak fitting algorithm
-  if (m_method == "centroid") {
-    peakFittingAlgorithm_ = new Centroid(true);
-  } else if (m_method == "gaussian") {
-    peakFittingAlgorithm_ = new FastGaussian();
-  } else {
-    std::cout << "ERROR: peak fitting method not supported!" << std::endl;
-    return std::vector<NeutronEvent>();
+    std::cout << clusterLabels_.size() << " " << data.size() << std::endl;
+    throw std::runtime_error("ERROR: cluster labels size does not match data!");
   }
 
   // Group data based on cluster labels
@@ -112,7 +91,7 @@ std::vector<NeutronEvent> ABS::get_events(const std::vector<Hit>& data) {
   auto max_label_it =
       std::max_element(clusterLabels_.begin(), clusterLabels_.end());
   int max_label = *max_label_it;
-#pragma omp parallel for
+  // #pragma omp parallel for
   for (int label = 0; label <= max_label; label++) {
     std::vector<Hit> cluster;
     for (size_t i = 0; i < data.size(); i++) {
@@ -121,9 +100,17 @@ std::vector<NeutronEvent> ABS::get_events(const std::vector<Hit>& data) {
       }
     }
     // get the neutron event
-    NeutronEvent event = peakFittingAlgorithm_->fit(cluster);
+    PeakFittingAlgorithm* alg;
+    if (m_method == "centroid") {
+      alg = new Centroid(true);
+    } else if (m_method == "gaussian") {
+      alg = new FastGaussian();
+    } else {
+      throw std::runtime_error("ERROR: peak fitting method not supported!");
+    }
+    auto event = alg->fit(cluster);
     // Add the event to the list
-#pragma omp critical
+    // #pragma omp critical
     events.push_back(event);
   }
 
