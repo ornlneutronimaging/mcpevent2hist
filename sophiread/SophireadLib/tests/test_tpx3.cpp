894537c876e698ac201e3d94d5eac388690d27a6
Chen Zhang <zhangc@ornl.gov>
2023-05-24 11:21:53 -0400

    move SophireadLib to sub folder

--- sophiread/SophireadLib/tests/test_tpx3.cpp
+++ sophiread/SophireadLib/tests/test_tpx3.cpp
@@ -7,7 +7,7 @@
 TEST(FileHandlingTest, ReadTPX3RawData) {
   // read the testing raw data
   auto hits =
-      readTimepix3RawData("data/frames_pinhole_3mm_1s_RESOLUTION_000001.tpx3");
+      readTimepix3RawData("../data/frames_pinhole_3mm_1s_RESOLUTION_000001.tpx3");
 
   // check the number of hits
   EXPECT_EQ(hits.size(), 9933804);
@@ -32,7 +32,7 @@ TEST(FileHandlingTest, ReadTPX3RawData) {
 TEST(FileHandlingTest, VerifyTiming) {
   // read the testing raw data
   auto hits =
-      readTimepix3RawData("data/greg_220ms_TDC_GDC_enabled_20230214_3.tpx3");
+      readTimepix3RawData("../data/greg_220ms_TDC_GDC_enabled_20230214_3.tpx3");
 
   // check the number of hits
   EXPECT_EQ(hits.size(), 365);
@@ -57,7 +57,7 @@ TEST(FileHandlingTest, VerifyTiming) {
 TEST(FileHandlingTest, VerifyRollover){
   // reading the testing raw data 
   auto hits =
-      readTimepix3RawData("data/rollover_test_data.tpx3");
+      readTimepix3RawData("../data/rollover_test_data.tpx3");
 
   // check the number of hits
   EXPECT_EQ(hits.size(),26);
@@ -102,8 +102,8 @@ TEST(FileHandlingTest, VerifyRollover){
 TEST(FileHandlingTest, ParseUserDefinedParams) {
 
   // read user-defined param files 
-  auto p1 = parseUserDefinedParams("../user_defined_params.txt");
-  auto p2 = parseUserDefinedParams("../user_defined_params_1.txt");
+  auto p1 = parseUserDefinedParams("../data/user_defined_params.txt");
+  auto p2 = parseUserDefinedParams("../data/user_defined_params_1.txt");
 
   // check user-defined params 
   EXPECT_EQ(p1.getABSRadius(), 5.0);
