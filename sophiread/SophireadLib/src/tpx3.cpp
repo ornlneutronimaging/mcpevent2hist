9c87c0d4712161fed8443486368ea934fb96462f
Chen Zhang <zhangc@ornl.gov>
2023-08-30 10:45:56 -0400

    add single thread raw2hit benchmark

--- sophiread/SophireadLib/src/tpx3.cpp
+++ sophiread/SophireadLib/src/tpx3.cpp
@@ -330,15 +330,103 @@ std::vector<Hit> readTimepix3RawData(const std::string &filepath) {
 }
 
 /**
+ * @brief Iterate through the raw char array and convert it to hits.
+ *
+ * @param raw_bytes
+ * @return std::vector<Hit>
+ */
+std::vector<Hit> parseRawBytesToHits(const std::vector<char> &raw_bytes) {
+  // check if the raw_bytes is empty
+  if (raw_bytes.size() == 0) {
+    throw std::runtime_error("Empty raw_bytes");
+  }
+
+  // assume every 100 char will result in 1 hit (this needs to be verified via
+  // experiments).
+  std::vector<Hit> hits;
+  hits.reserve(raw_bytes.size() / 100);
+
+  // local variables
+  // -- packet information
+  int chip_layout_type = 0;
+  int data_packet_size = 0;
+  int data_packet_num = 0;
+  // -- TDC
+  unsigned long *tdclast;
+  unsigned long long mytdc = 0;
+  unsigned long TDC_MSB16 = 0;
+  unsigned long TDC_LSB32 = 0;
+  unsigned long TDC_timestamp = 0;
+  // -- GDC
+  unsigned long *gdclast;
+  unsigned long long mygdc = 0;
+  unsigned long Timer_LSB32 = 0;
+  unsigned long Timer_MSB16 = 0;
+  unsigned long long int GDC_timestamp = 0;  // 48-bit
+
+  // main loop
+  for (auto i = raw_bytes.begin(); i < raw_bytes.end(); i += 8) {
+    const auto char_array = std::vector<char>(i, std::next(i, 8));
+    // locate the data packet header
+    if (char_array[0] == 'T' && char_array[1] == 'P' && char_array[2] == 'X') {
+      // get the size of the data packet
+      data_packet_size = ((0xff & char_array[7]) << 8) | (0xff & char_array[6]);
+      data_packet_num = data_packet_size >> 3;  // every 8 (2^3) bytes is a data packet
+      // get chip layout type
+      chip_layout_type = (int)char_array[4];
+
+      // processing each data packet
+      for (auto j = 0; j < data_packet_num; j++) {
+        i = std::next(i, 8);  // move the iterator to the next data packet
+        std::vector<char> data_packet(i, std::next(i, 8));
+        if (data_packet.size() != 8) {
+          std::cout << "EOF, insufficient bytes left." << std::endl;
+          break;
+        }
+        // extract the data from the data packet
+        if (data_packet[7] == 0x6F) {
+          // TDC data packets
+          tdclast = (unsigned long *)(&data_packet[0]);
+          mytdc = (((*tdclast) >> 12) & 0xFFFFFFFF);  // rick: 0x3fffffff, get 32-bit tdc
+          TDC_LSB32 = GDC_timestamp & 0xFFFFFFFF;
+          TDC_MSB16 = (GDC_timestamp >> 32) & 0xFFFF;
+          if (mytdc < TDC_LSB32) {
+            TDC_MSB16++;
+          }
+          TDC_timestamp = (TDC_MSB16 << 32) & 0xFFFF00000000;
+          TDC_timestamp = TDC_timestamp | mytdc;
+        } else if ((data_packet[7] & 0xF0) == 0x40) {
+          // GDC data packet
+          gdclast = (unsigned long *)(&data_packet[0]);
+          mygdc = (((*gdclast) >> 16) & 0xFFFFFFFFFFF);
+          if (((mygdc >> 40) & 0xF) == 0x4) {
+            Timer_LSB32 = mygdc & 0xFFFFFFFF;  // 32-bit
+          } else if (((mygdc >> 40) & 0xF) == 0x5) {
+            Timer_MSB16 = mygdc & 0xFFFF;  // 16-bit
+            GDC_timestamp = Timer_MSB16;
+            GDC_timestamp = (GDC_timestamp << 32) & 0xFFFF00000000;
+            GDC_timestamp = GDC_timestamp | Timer_LSB32;
+          }
+        } else if ((data_packet[7] & 0xF0) == 0xb0) {
+          // Process the data into hit
+          auto hit = packetToHit(data_packet, TDC_timestamp, GDC_timestamp, chip_layout_type);
+          hits.push_back(hit);
+        }
+      }
+    }
+  }
+  // Return the hits
+  return hits;
+}
+
+/**
  * @brief Save labeled hits to HDF5 file.
  *
  * @param out_file_name: output file name.
  * @param hits: hits to be saved.
  * @param labels: cluster ID for each hits.
  */
-void saveHitsToHDF5(const std::string out_file_name,
-                    const std::vector<Hit> &hits,
-                    const std::vector<int> &labels) {
+void saveHitsToHDF5(const std::string out_file_name, const std::vector<Hit> &hits, const std::vector<int> &labels) {
   // sanity check
   if (hits.size() != labels.size()) {
     throw std::runtime_error("Hits and labels must have the same size");
@@ -355,54 +443,42 @@ void saveHitsToHDF5(const std::string out_file_name,
   H5::Group group = out_file.createGroup("hits");
   // -- write x
   std::vector<int> x(hits.size());
-  std::transform(hits.begin(), hits.end(), x.begin(),
-                 [](const Hit &hit) { return hit.getX(); });
+  std::transform(hits.begin(), hits.end(), x.begin(), [](const Hit &hit) { return hit.getX(); });
   H5::DataSet x_dataset = group.createDataSet("x", int_type, dataspace);
   x_dataset.write(x.data(), int_type);
   // -- write y
   std::vector<int> y(hits.size());
-  std::transform(hits.begin(), hits.end(), y.begin(),
-                 [](const Hit &hit) { return hit.getY(); });
+  std::transform(hits.begin(), hits.end(), y.begin(), [](const Hit &hit) { return hit.getY(); });
   H5::DataSet y_dataset = group.createDataSet("y", int_type, dataspace);
   y_dataset.write(y.data(), int_type);
   // -- write tot_ns
   std::vector<double> tot_ns(hits.size());
-  std::transform(hits.begin(), hits.end(), tot_ns.begin(),
-                 [](const Hit &hit) { return hit.getTOT_ns(); });
-  H5::DataSet tot_ns_dataset =
-      group.createDataSet("tot_ns", float_type, dataspace);
+  std::transform(hits.begin(), hits.end(), tot_ns.begin(), [](const Hit &hit) { return hit.getTOT_ns(); });
+  H5::DataSet tot_ns_dataset = group.createDataSet("tot_ns", float_type, dataspace);
   tot_ns_dataset.write(tot_ns.data(), float_type);
   // -- write toa_ns
   std::vector<double> toa_ns(hits.size());
-  std::transform(hits.begin(), hits.end(), toa_ns.begin(),
-                 [](const Hit &hit) { return hit.getTOA_ns(); });
-  H5::DataSet toa_ns_dataset =
-      group.createDataSet("toa_ns", float_type, dataspace);
+  std::transform(hits.begin(), hits.end(), toa_ns.begin(), [](const Hit &hit) { return hit.getTOA_ns(); });
+  H5::DataSet toa_ns_dataset = group.createDataSet("toa_ns", float_type, dataspace);
   toa_ns_dataset.write(toa_ns.data(), float_type);
   // -- write ftoa_ns
   std::vector<double> ftoa_ns(hits.size());
-  std::transform(hits.begin(), hits.end(), ftoa_ns.begin(),
-                 [](const Hit &hit) { return hit.getFTOA_ns(); });
-  H5::DataSet ftoa_ns_dataset =
-      group.createDataSet("ftoa_ns", float_type, dataspace);
+  std::transform(hits.begin(), hits.end(), ftoa_ns.begin(), [](const Hit &hit) { return hit.getFTOA_ns(); });
+  H5::DataSet ftoa_ns_dataset = group.createDataSet("ftoa_ns", float_type, dataspace);
   ftoa_ns_dataset.write(ftoa_ns.data(), float_type);
   // -- write tof_ns
   std::vector<double> tof_ns(hits.size());
-  std::transform(hits.begin(), hits.end(), tof_ns.begin(),
-                 [](const Hit &hit) { return hit.getTOF_ns(); });
-  H5::DataSet tof_ns_dataset =
-      group.createDataSet("tof_ns", float_type, dataspace);
+  std::transform(hits.begin(), hits.end(), tof_ns.begin(), [](const Hit &hit) { return hit.getTOF_ns(); });
+  H5::DataSet tof_ns_dataset = group.createDataSet("tof_ns", float_type, dataspace);
   tof_ns_dataset.write(tof_ns.data(), float_type);
   // -- write spidertime_ns
   std::vector<double> spidertime_ns(hits.size());
   std::transform(hits.begin(), hits.end(), spidertime_ns.begin(),
                  [](const Hit &hit) { return hit.getSPIDERTIME_ns(); });
-  H5::DataSet spidertime_ns_dataset =
-      group.createDataSet("spidertime_ns", float_type, dataspace);
+  H5::DataSet spidertime_ns_dataset = group.createDataSet("spidertime_ns", float_type, dataspace);
   spidertime_ns_dataset.write(spidertime_ns.data(), float_type);
   // -- write labels
-  H5::DataSet labels_dataset =
-      group.createDataSet("labels", int_type, dataspace);
+  H5::DataSet labels_dataset = group.createDataSet("labels", int_type, dataspace);
   labels_dataset.write(labels.data(), int_type);
   // -- close file
   out_file.close();
@@ -414,8 +490,7 @@ void saveHitsToHDF5(const std::string out_file_name,
  * @param out_file_name: output file name.
  * @param events: neutron events to be saved.
  */
-void saveEventsToHDF5(const std::string out_file_name,
-                      const std::vector<NeutronEvent> &events) {
+void saveEventsToHDF5(const std::string out_file_name, const std::vector<NeutronEvent> &events) {
   // sanity check
   if (events.size() == 0) {
     throw std::runtime_error("No events to save");
@@ -432,22 +507,19 @@ void saveEventsToHDF5(const std::string out_file_name,
   H5::Group group = out_file.createGroup("events");
   // -- write x
   std::vector<double> x(events.size());
-  std::transform(events.begin(), events.end(), x.begin(),
-                 [](const NeutronEvent &event) { return event.getX(); });
+  std::transform(events.begin(), events.end(), x.begin(), [](const NeutronEvent &event) { return event.getX(); });
   H5::DataSet x_dataset = group.createDataSet("x", float_type, dataspace);
   x_dataset.write(x.data(), float_type);
   // -- write y
   std::vector<double> y(events.size());
-  std::transform(events.begin(), events.end(), y.begin(),
-                 [](const NeutronEvent &event) { return event.getY(); });
+  std::transform(events.begin(), events.end(), y.begin(), [](const NeutronEvent &event) { return event.getY(); });
   H5::DataSet y_dataset = group.createDataSet("y", float_type, dataspace);
   y_dataset.write(y.data(), float_type);
   // -- write TOF_ns
   std::vector<double> tof_ns(events.size());
   std::transform(events.begin(), events.end(), tof_ns.begin(),
                  [](const NeutronEvent &event) { return event.getTOF_ns(); });
-  H5::DataSet tof_ns_dataset =
-      group.createDataSet("tof_ns", float_type, dataspace);
+  H5::DataSet tof_ns_dataset = group.createDataSet("tof_ns", float_type, dataspace);
   tof_ns_dataset.write(tof_ns.data(), float_type);
   // -- write Nhits
   std::vector<int> nhits(events.size());
@@ -455,51 +527,49 @@ void saveEventsToHDF5(const std::string out_file_name,
                  [](const NeutronEvent &event) { return event.getNHits(); });
   H5::DataSet nhits_dataset = group.createDataSet("NHits", int_type, dataspace);
   nhits_dataset.write(nhits.data(), int_type);
-  // -- write TOT 
+  // -- write TOT
   std::vector<double> tot(events.size());
-  std::transform(events.begin(),events.end(),tot.begin(),
-                [](const NeutronEvent &event) { return event.getTOT(); });
+  std::transform(events.begin(), events.end(), tot.begin(), [](const NeutronEvent &event) { return event.getTOT(); });
   H5::DataSet tot_dataset = group.createDataSet("tot", float_type, dataspace);
   tot_dataset.write(tot.data(), float_type);
   // -- close file
   out_file.close();
 }
 
-/**
- * @brief Parse user-defined parameters from a parameter file 
- *
- * @param filepath: path to the parameter file.
- * @return Params
- */
+  /**
+   * @brief Parse user-defined parameters from a parameter file
+   *
+   * @param filepath: path to the parameter file.
+   * @return Params
+   */
 
-Params parseUserDefinedParams(const std::string& filepath){
-  // default ABS settings 
-  double radius = 5.0;
-  unsigned long int min_cluster_size = 1;
-  unsigned long int spider_time_range = 75;
+  Params parseUserDefinedParams(const std::string &filepath) {
+    // default ABS settings
+    double radius = 5.0;
+    unsigned long int min_cluster_size = 1;
+    unsigned long int spider_time_range = 75;
 
-  std::ifstream user_defined_params_file(filepath);
-  std::string line;
+    std::ifstream user_defined_params_file(filepath);
+    std::string line;
 
-  while (std::getline(user_defined_params_file,line)){
-    std::istringstream ss(line);
-    std::string name;
-    ss >> name;
-    if (name == "abs_radius"){
+    while (std::getline(user_defined_params_file, line)) {
+      std::istringstream ss(line);
+      std::string name;
+      ss >> name;
+      if (name == "abs_radius") {
       ss >> radius;
-    } else if (name == "abs_min_cluster_size"){
+      } else if (name == "abs_min_cluster_size") {
       ss >> min_cluster_size;
-    } else if (name == "spider_time_range"){
+      } else if (name == "spider_time_range") {
       ss >> spider_time_range;
+      }
     }
+
+    Params p(radius, min_cluster_size, spider_time_range);
+
+    // prints out user-defined parameters
+    std::cout << "User-defined params file: " << filepath << std::endl;
+    std::cout << p.toString() << std::endl;
+
+    return p;
   }
-
-  Params p(radius,min_cluster_size,spider_time_range);
-
-  // prints out user-defined parameters 
-  std::cout << "User-defined params file: " << filepath << std::endl;
-  std::cout << p.toString() << std::endl;
-
-  return p;
-
-}
